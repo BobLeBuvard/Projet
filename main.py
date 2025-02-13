5d09d603b2f5415d2374359a23a3f9a8910fcedb
Pierre Sluse <pierresluse@gmail.com>
2025-02-13 10:43:19 +0100

    Ajout de fichiers utiles et autres portions de code pratiques (degrés en kelvin , énoncé, PerteEtGAin.txt, etc...)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-
+import numpy as np
+import scipy as scp
+import matplotlib.pyplot as plt
 #VARIABLES
 T_room = 293,15 #kelvins
 T_c1 = 293,15 #kelvins
@@ -6,19 +8,24 @@ T_c2 = 293,15 #kelvins
 T_cc = 293,15 #kelvins
 T_t = 293,15 #kelvins
 
+PERTE_ET_GAIN = np.loadtxt('PerteEtGain.txt') # array constante du fichier texte 
 
 
+
+def celcius_en_kelvin(Temp_celcius):
+    '''fonction qui transforme une température en degés celcius en une température en degrés kelvin'''
+    return (Temp_celcius + 273.15)
 
 def Coeff_performance_chaud(temp_in, temp_out):
     '''calcule le coefficient de performance d'une pompe à chaleur
     TOUTES LES TEMPERATURES EN KELVINS 
     temp_in est la température extérieure (la plus froide)
     temp_out est la température intérieure (la plus chaude)'''
-    return(0,5*temp_in/(temp_in - temp_out))
+    return(0.5*temp_out/(temp_in - temp_out))
 
 def Coeff_performance_froid(temp_in, temp_out):
     '''calcule le coefficient de performance d'une machine à froid'''
-    return(0,5*temp_out/(temp_in - temp_out))
+    return(0.5*temp_in/(temp_in - temp_out))
 
 def T_optimale(T_room, T_surface):
     '''calcule la température ressentie en fonction de la chaleur de la pièce et celles des surfaces'''
