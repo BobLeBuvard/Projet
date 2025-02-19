924a7ad24cc37b55c141bb35f09689c4d063e2e2
“[Louis <[meanlouis72222@gmail.com]>
2025-02-19 22:55:26 +0100

    mise des équations diff dans l'ordre (oui je suis timbré, instable hein)

--- PerteEtGain.py
+++ PerteEtGain.py
@@ -5,6 +5,7 @@ PERTE_ET_GAIN = np.loadtxt('PerteEtGain.txt') # array constante du fichier texte
 heures = PERTE_ET_GAIN[0]
 flux = PERTE_ET_GAIN[1]
 val = scp.interpolate.CubicSpline(heures,flux,bc_type='clamped')
-val2 = scp.interpolate.UnivariateSpline(heures,flux,s = 10)
-#plt.plot(heures, val(heures),label='Spline')
+val2 = scp.interpolate.UnivariateSpline(heures,flux,s = 5)
+plt.plot(heures, val(heures),label='Spline')
 plt.plot(heures, val2(heures),label='Spline')
+plt.show()
