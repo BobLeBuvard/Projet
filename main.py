cb8d1b4d48d588c094d4d1dae78688083aa0e59b
“[Louis <[meanlouis72222@gmail.com]>
2025-02-19 15:52:39 +0100

    Mis à jour des valeurs des variables & noms

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ C_c1 = 50 # Capacité de la partie supérieure béton
 C_c2 = 10 # Capacité de la partie inférieure béton
 C_cc = 50 # Capacité de la partie centrale béton
 C_t = 1 # Capacité des tubes
-C_w = 30 
+C_w = 30 # Capacité de l'eau
 R_s = 1 #Résistance de la surface entrre le béton et la pièce régulée
 R_x = 0.025 #Résistance de contact entre les tubes & la partie centrale du béton (m²K/W)
+R_w = 0.15 #Résistance de l'eau
 R_cc_moins_c1 = 0.025
 R_c2_moins_cc = 0.02
 R_r_moins_s = 0.1
 R_s_moins_c2 = 0.183
-R_w = 0.15
+
 
 T = np.array([T_room, T_t, T_cc, T_c1,T_c2])
 C = np.array([C_room, C_t, C_cc, C_c1,C_c2])
