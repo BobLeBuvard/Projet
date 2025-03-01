a82986546785b42a8a2202d043588b024407931a
“[Louis <[meanlouis72222@gmail.com]>
2025-03-01 13:02:19 +0100

    4.2 début

--- Question4.py
+++ Question4.py
@@ -1,6 +1,7 @@
 from SimTABS import calculTemperaturesEuler
 import math
 from RechercheRacine import hasRoots
+from RechercheRacine import bissection
 import numpy as np
 from SimTabsFinal import calculTemperaturesEuler
 
@@ -46,54 +47,20 @@ def T_max(delta_t, T_d_max):
 #______________________________________________________________________________________________________#
 #question 4.2
 
-def bissection_secante(f, x0, x1, tol = 0.5e-7, max_iter=50): #par défaut une tolérance de 8 décimales correctes, et un nombre d'itérations max de 50 
-    '''
-    Recherche de racine par dichotomie -> on coupe l'intervalle en deux et on regarde le signe de ce terme pour diminuer la taille de l'intervalle d'un facteur 1/2
-
-    une fois que les limites de l'intervalle sont suffisamment proches de zéro, on retourne la valeur de x à cet endroit ansi qu'un code d'erreur s'il y a eu un problème.
+def recherche_delta_t(T_max_d,x0,xf,tol = 0.5e-7)
     
-    f : fonction d'entrée
+ f_difference = lambda deltaT: T_max(deltaT) - T_max_d 
+ delta_t  = bissection(f_difference, 0 , 24 , 0.5e-7, max_iter=54)
+return delta_t
 
-    x0: début (ou fin) de l'intervalle de recherche de racine
 
-    x1: fin (ou début) de l'intervalle de recherche de racine
+'''''
 
-    tol: tolérance de l'imprécision de la racine (par défaut 0.5e-7)
-
-    max_iter: nombre maximal d'itérations de la recherche
-
-    
-    Sortie sous la forme:
-
-    [x, status]
-
-    '''
-    retour = hasRoots(f,x0,x1,tol)
-    if retour[1] != 0:
-        return retour
-    #CODE EN NEGLIGEANT LES FONCTION AVEC nbr_racines > 1  en nombre pairs ( souci pour 3 racines, 5 racines,... )
-    
-    
-    nombre_d_iterations = math.ceil(np.log2((x1 - x0) / (2 * tol))) #arrondi supérieur
-
-    if(nombre_d_iterations <= 0 or nombre_d_iterations > max_iter):
-        return [1984,-1] # on a un souci de convergeance: un nombre négatif d'itérations...
-
-    for i in range(nombre_d_iterations):
-        x2 = (x0+x1) /2 #creation d'un point au milieu de x0 et x1 
-        fx2 = f(x2)     #STOCKAGE DE L'ESTIMATION DE LA FONCTION A UN POINT X2
-        fx0 = f(x0)     #STOCKAGE DE L'ESTIMATION DE LA FONCTION A UN POINT X0
-        if fx2 < tol:
-            return [x2 , 0] #on a trouvé la racine exacte -> on quitte
-        elif(fx0 * fx2 < 0): #on compare le signe de x0 et x2 
-            x1 = x2 # f(x0) et f(x2) sont de signe contraire -> le zéro se trouve entre x0 et x2
-        else: 
-            x0 = x2 # les deux nombres sont de même signe -> le zéro se trouve entre x2 et x1
         if i > math.ceil((max_iter)/4): 
             break
 
 
-    #passage à la méthode de la sécante 
+#passage à la méthode de la sécante 
 
        
     fx1 = f(x1)     #STOCKAGE DE L'ESTIMATION DE LA FONCTION A UN POINT X1
@@ -108,9 +75,9 @@ def bissection_secante(f, x0, x1, tol = 0.5e-7, max_iter=50): #par défaut une t
         return [x2, 0] #on a trouvé la racine correcte (avec tolérances en valeur absolue) !
     
     x0, x1 = x1, x2  # Sinon on met les valeurs à jour x1 devient x0 et x2 devient x1  
-    return [x2,0]
+    return [x2,0]   
 
-
+'''
 
 #______________________________________________________________________________________________________#
 # question 4.3
